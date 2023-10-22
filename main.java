b9e8e286571926bd965837eeaade09aa5247c9b0
XCFC <124226344+menafec@users.noreply.github.com>
2023-10-22 11:01:24 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Seleccione el tipo de regresion:");
-        System.out.println("1. Regresion lineal");
-        System.out.println("2. Regresion lineal cuadratica");
-        System.out.println("3. Regresion lineal multiple");
+        // Muestra las opciones de regresión disponibles
+        System.out.println("Seleccione el tipo de regresión:");
+        System.out.println("1. Regresión lineal");
+        System.out.println("2. Regresión lineal cuadrática");
+        System.out.println("3. Regresión lineal múltiple");
+        System.out.println("4. Regresión polinomial");
 
+        // Lee la opción seleccionada por el usuario
         int opcion = scanner.nextInt();
 
+        // Ejecuta el programa correspondiente según la opción seleccionada
         switch (opcion) {
             case 1:
                 regresionlineal.main(args);
@@ -20,6 +25,9 @@ public class main {
                 break;
             case 3:
                 RegresionLinealMultiple.main(args);
+                break;
+            case 4:
+                RegresionPolinomial.main(args);
                 break;
             default:
                 System.out.println("Opción inválida");
